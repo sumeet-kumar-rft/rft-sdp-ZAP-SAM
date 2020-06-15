7926e0ca99b9f1c7448889d3e4c7fe6a81792764
sumeet-kumar-rft <sumeetkumar1@refinitiv.com>
2020-06-15 19:44:43 +0530

    for list of url's

---
+++ tasks/dast-2nd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+if [ "$DISABLED" = true ]; then
+    echo "This stage has been disabled"
+    exit
+fi
+echo "Performing DAST scanning"
+echo "Targets:"
+cat ./pipeline-git/tasks/target-data/target.txt
+mkdir -p /zap/wrk
+for host in $(cat ./pipeline-git/tasks/target-data/target.txt)
+do
+    cd /tmp
+  # workaround to fix hanging Zap processes that prevents the container from exiting - https://github.com/concourse/concourse/issues/763
+  Xvfb :1 -screen 0 1024x768x16 -ac &
+  XVFB_PID=$!
+  echo "SCANNING $host"
+    zap-baseline.py -t $host -g gen.conf -r "report.html" -J "report.json" 
+  # workaround to fix hanging Zap processes that prevents the container from exiting - https://github.com/concourse/concourse/issues/763
+  kill $XVFB_PID
+  echo "DAST SCANNING COMPLETE"
+done
