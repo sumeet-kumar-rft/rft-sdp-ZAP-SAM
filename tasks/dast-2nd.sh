6ad77cd3fddfdb3b460d90a05eeecddc6b381020
Hasitha Ubeywarna <hasitha.ubeywarna@refinitiv.com>
2020-07-28 10:26:19 +0100

    changes

--- tasks/dast-2nd.sh
+++ tasks/dast-2nd.sh
@@ -7,7 +7,7 @@ echo "Performing DAST scanning"
 echo "Targets:"
 cat ./pipeline-git/tasks/target-data/target.txt
 mkdir -p /zap/wrk
-set -o monitor
+#set -o monitor
 #trap 'exit(0)' CHLD
 for host in $(cat ./pipeline-git/tasks/target-data/target.txt)
 do
@@ -16,6 +16,6 @@ do
   echo "SCANNING $host"
   zap-baseline.py -t $host -g gen.conf -r "report.html" -J "report.json" 
   # workaround to fix hanging Zap processes that prevents the container from exiting - https://github.com/concourse/concourse/issues/763
-  echo "DAST SCANNING COMPLETE"
+  echo "==DAST SCANNING COMPLETE=="
 done
 #kill `pgrep Xvfb`
